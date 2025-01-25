6eec150d7fcef55ca5ae16205bdf8e3b81e4f7b0
Ramyasri135 <kurvaramyasri4242@gmail.com>
2025-01-25 09:53:10 +0530

    Add files via upload

---
+++ Areaofrect.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+    public class Areaofrect {
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("enter length: ");
+            int a=sc.nextInt();
+            System.out.println("enter breadth: ");
+            float b=sc.nextFloat();
+            double c=a*b;
+            System.out.println("area of rectangle:"+c+" cm2");
+        }
+    }
+    
+
